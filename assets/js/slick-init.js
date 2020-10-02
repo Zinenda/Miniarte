eada7408bab81c517870613d62763a335270aa78
Ivo Zinenda <izinenda@gmail.com>
2020-10-02 13:12:44 +0200

    portfolio section

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -2,6 +2,43 @@ $(document).ready(function(){
 
 	$('.sobre-carousel').slick();
 
+	$('.slick-servicos').slick({
+		dots: true,
+		infinite: false,
+		lazyLoad: 'ondemand',
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 3,
+			  slidesToScroll: 3,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		  // You can unslick at a given breakpoint now by adding:
+		  // settings: "unslick"
+		  // instead of a settings object
+		]
+	});
+
     $('.slick-clientes').slick({
 		dots: false,
 		lazyLoad: 'ondemand',
